d8904dd2f74ec78da4bc6132878674c130c8918e
Shivam Sharma <shivam.sharma.ug21@nsut.ac.in>
2024-05-05 21:16:21 +0530

    certificates added

---
+++ src/components/sections/Certificates.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+import { certificactions } from "../../constants";
+import { TCertification } from "../../types";
+import { SectionWrapper } from "../../hoc";
+import { fadeIn } from "../../utils/motion";
+import { config } from "../../constants/config";
+import { Header } from "../atoms/Header";
+import { Link } from "react-router-dom";
+
+interface ICertificateCard {
+  index: number;
+  testimonial: string;
+  name: string;
+  designation: string;
+  company: string;
+  // icon: string;
+  // link: string;
+  image: string;
+}
+
+const CertificateCard: React.FC<ICertificateCard> = ({ index,
+  testimonial,
+  name,
+  designation,
+  company,
+  image, }) => (
+  <Tilt
+    glareEnable
+    tiltEnable
+    tiltMaxAngleX={30}
+    tiltMaxAngleY={30}
+    glareColor="#aaa6c3"
+  >
+    <Link to={designation} target="_blank" rel="noopener noreferrer">
+      <div className="xs:w-[355px] w-full" >
+        <motion.div
+          variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+          className="green-pink-gradient shadow-card w-full rounded-[20px] p-[1px]"
+        >
+          <div className="bg-tertiary flex min-h-[320px] flex-col items-center justify-evenly rounded-[20px] px-12 py-5">
+            <img
+              src={image}
+              alt="certificate"
+              className="h-48 w-96"
+            />
+
+            <h3 className="text-center text-[20px] font-bold text-white">
+              {name}
+            </h3>
+            <h2 className="text-center text-[16px] font-bold text-white">
+              {testimonial}
+            </h2>
+            <h2 className="text-center text-[16px] font-bold text-white">
+              {company}
+            </h2>
+          </div>
+        </motion.div>
+      </div>
+    </Link>
+  </Tilt>
+);
+
+const Certificates = () => {
+  return (
+    <>
+      <Header useMotion={true} {...config.sections.feedbacks} />
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="text-secondary mt-4 max-w-3xl text-[17px] leading-[30px]"
+      >
+        {/* {config.sections.about.content} */}
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10 max-sm:justify-center">
+        {certificactions.map((certificate, index) => (
+          <CertificateCard key={certificate.name} index={index} {...certificate} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Certificates, "certificates");
---
+++ src/constants/index.ts
@@ -0,0 +1,325 @@
+import type {
+  TNavLink,
+  TService,
+  TTechnology,
+  TExperience,
+  TTestimonial,
+  TCertification,
+  TProject,
+} from "../types";
+import javascript_one from '../assets/certificates/jsOne.png'
+import javascript_two from '../assets/certificates/jsTwo.png'
+import react_one from '../assets/certificates/reactOne.png'
+import python_one from '../assets/certificates/pythonOne.png'
+import problem_solving_one from '../assets/certificates/psOne.png'
+import problem_solving_two from '../assets/certificates/psTwo.png'
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks: TNavLink[] = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "certificates",
+    title: "Certificates",
+  },
+  {
+    id: "works",
+    title: "Projects",
+  },
+];
+
+const services: TService[] = [
+  {
+    title: "Full-Stack Developer",
+    icon: web,
+  },
+  {
+    title: "Web Developer",
+    icon: mobile,
+  },
+  {
+    title: "UI/UX Designer",
+    icon: backend,
+  },
+  {
+    title: "Software enthusiast Student",
+    icon: creator,
+  },
+];
+
+const technologies: TTechnology[] = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences: TExperience[] = [
+  {
+    title: "React.js Developer",
+    companyName: "Starbucks",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    companyName: "Tesla",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Jan 2021 - Feb 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    companyName: "Shopify",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    companyName: "Meta",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials: TTestimonial[] = [
+  {
+    testimonial:
+      "JavaScript",
+    name: "Advanced",
+    designation: "HackerRank",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const certificactions: TCertification[] = [
+  {
+    testimonial:
+      "Basic",
+    name: "JavaScript",
+    designation: "https://www.hackerrank.com/certificates/95a213835cef",
+    company: "HackerRank",
+    image: javascript_one,
+  },
+  {
+    testimonial:
+      "Intermediate",
+    name: "JavaScript",
+    designation: "https://www.hackerrank.com/certificates/97e41b53e846",
+    company: "HackerRank",
+    image: javascript_two,
+  },
+  {
+    testimonial:
+      "Basic",
+    name: "Problem solving",
+    designation: "https://www.hackerrank.com/certificates/95a213835cef",
+    company: "HackerRank",
+    image: problem_solving_one,
+  },
+  {
+    testimonial:
+      "Intermediate",
+    name: "Problem Solving",
+    designation: "https://www.hackerrank.com/certificates/fdef29a252d0",
+    company: "HackerRank",
+    image: problem_solving_two,
+  },
+  {
+    testimonial:
+      "Basic",
+    name: "ReactJS",
+    designation: "https://www.hackerrank.com/certificates/c5e11fdf8ed2",
+    company: "HackerRank",
+    image: react_one,
+  },
+  {
+    testimonial:
+      "Basic",
+    name: "Python",
+    designation: "https://www.hackerrank.com/certificates/2edf8dd29bba",
+    company: "HackerRank",
+    image: python_one,
+  },
+];
+
+const projects: TProject[] = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    sourceCodeLink: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects, certificactions };
